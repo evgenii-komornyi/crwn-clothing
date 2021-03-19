9dddf3139f6f57892e919bce42f95ab9888ce472
evgenii-komornyi <evgeniikomornyi@gmail.com>
2021-03-19 14:41:14 +0200

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey =
+        'pk_test_51IWIUiLtjBgXtFSlRZkOOSISeZawhQDGeV9LGcLog19b1xHx4uc0WnZVWbGs8nSUJAKfUJSsFy58q1NJ1LfnT4x100ZEwNQrNs';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment successful');
+    };
+
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="CRWN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
