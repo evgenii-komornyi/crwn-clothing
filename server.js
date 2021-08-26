b02a07bd5eb2c30cc73e4f8b06f1b7b017a4df36
evgenii-komornyi <evgeniikomornyi@gmail.com>
2021-08-26 15:12:45 +0300

    pwa2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const compression = require('compression');
 const enforce = require('express-sslify');
 
@@ -11,11 +12,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
